3a15f9887c1ae1532953403c842724a94f15a728
brianhoyer03 <brianhurtado653@gmail.com>
2024-07-09 12:31:18 -0500

    Agregando mas codigo a la pagina web con python

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Home Page   '
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
